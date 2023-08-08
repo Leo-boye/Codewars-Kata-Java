c775492470a09ce87ea38e04d6492aa141eaa00e
Leo-boye <leo.boye@epita.fr>
2023-08-08 23:33:22 +0200

    Exercise kyu6: Count the smiley faces!
    update solution:
     - gain in performance
     - easier to read

--- src/main/java/kyu6/SmileFaces.java
+++ src/main/java/kyu6/SmileFaces.java
@@ -5,22 +5,6 @@ import java.util.*;
 public class SmileFaces {
 
     public static int countSmileys(List<String> arr) {
-
-        List<String> validSmileyList = new ArrayList<>();
-
-        String[] eyes = new String[]{";", ":"};
-        String[] noses = new String[]{"-", "~"};
-        String[] mouths = new String[]{"D", ")"};
-
-        for (String eye : eyes) {
-            for (String mouth : mouths) {
-                for (String nose : noses) {
-                    validSmileyList.add(eye.concat(nose).concat(mouth));
-                }
-                validSmileyList.add(eye.concat(mouth));
-            }
-        }
-
-        return (int) arr.stream().filter(validSmileyList::contains).count();
+        return (int) arr.stream().filter(smiley -> smiley.matches("[:;][-~]?[)D]")).count();
     }
 }
