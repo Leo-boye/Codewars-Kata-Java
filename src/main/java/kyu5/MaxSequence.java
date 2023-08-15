b8453dfb5621fe85974f1ea872dbc6e315e0c3c4
Leo-boye <leo.boye@epita.fr>
2023-08-15 15:47:40 +0200

    Exercise kyu5: Maximum subarray sum (TDD)
     - Real Example case
    
     https://www.codewars.com/kata/54521e9ec8e60bc4de000d6c

--- src/main/java/kyu5/MaxSequence.java
+++ src/main/java/kyu5/MaxSequence.java
@@ -4,6 +4,14 @@ import java.util.Arrays;
 
 public class MaxSequence {
     public static int sequence(int[] arr) {
-        return  Arrays.stream(arr).sum();
+        int bestSum = 0;
+        int currentSum = 0;
+
+        for (int element : arr) {
+            currentSum = Math.max(0, currentSum + element);
+            bestSum = Math.max(bestSum, currentSum);
+        }
+
+        return bestSum;
     }
 }
--- src/test/java/kyu5/MaxSequenceTest.java
+++ src/test/java/kyu5/MaxSequenceTest.java
@@ -15,4 +15,9 @@ public class MaxSequenceTest {
     public void testSimpleArray() throws Exception {
         assertEquals("Empty arrays should have a max of 5", 5, MaxSequence.sequence(new int[]{5}));
     }
+
+    @Test
+    public void testExampleArray() throws Exception {
+        assertEquals("Example array should have a max of 6", 6, MaxSequence.sequence(new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4}));
+    }
 }
